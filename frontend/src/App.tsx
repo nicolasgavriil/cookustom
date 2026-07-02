92255c1c6f9ec3c7113bfb54238c995678516d9a
nicolasgavriil <219146079+nicolasgavriil@users.noreply.github.com>
2026-07-02 20:18:33 +0200

    feat(frontend-recipes): add recipe detail page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ import { LoginPage } from './pages/LoginPage'
 import { NewIngredientPage } from './pages/NewIngredientPage'
 import { NotFoundPage } from './pages/NotFoundPage'
 import { RegisterPage } from './pages/RegisterPage'
+import { RecipeDetailPage } from './pages/RecipeDetailPage'
 import { RecipesPage } from './pages/RecipesPage'
 
 const App = () => {
@@ -47,6 +48,14 @@ const App = () => {
           element={
             <RequireAuth>
               <RecipesPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/recipes/:recipeId"
+          element={
+            <RequireAuth>
+              <RecipeDetailPage />
             </RequireAuth>
           }
         />
